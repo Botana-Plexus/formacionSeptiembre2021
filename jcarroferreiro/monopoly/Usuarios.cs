198879e52dc0ff0afb3e26eb34a677a78809aad0
Jose Manuel Carro Ferreiro <jose.carroferreiro@plexus.local>
2021-09-29 13:13:29 +0200

    VAAAAAAAMOS RAFA

---
+++ jcarroferreiro/monopoly/Usuarios.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace monopoly
+{
+    public class Usuarios
+    {
+        /*
+            Estructura de la tabla usuario de BBDD
+         */
+        public int id { get; set; }
+        public string email { get; set; }
+        public string nick { get; set; }
+        public string pass { get; set; }
+        public string fechaNacimiento { get; set; }
+
+    }
+    public class Autenticar{
+        public string email { get; set; }
+        public string pass { get; set; }
+    }
+    public class Tablero
+    {
+        public int id { get; set; }
+        public string descripcion { get; set; }
+        public int importe { get; set; }
+        public int numCasillas { get; set; }
+    }
+    public class CrearPartida
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public int admin { get; set; }
+        public int maxJugadores { get; set; }
+        public int maxTiempo { get; set; }
+        public string pass { get; set; }
+        public int  tablero { get; set; }
+    }
+    public class UnirsePartida
+    {
+        public int idUsuario { get; set; }
+        public int idPartida { get; set; }
+    }
+    public class ComenzarPartida
+    {
+        public int idPartida { get; set; }
+    }
+}
