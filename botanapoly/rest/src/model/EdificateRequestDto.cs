e63395cdbe8a4d40ceca8491986d2934c6edf50b
g-gar <g.gar.fern@hotmail.com>
2021-10-03 13:20:46 +0200

    finished Rest API DTOs

--- botanapoly/rest/src/model/EdificateRequestDto.cs
+++ botanapoly/rest/src/model/EdificateRequestDto.cs
@@ -1,6 +1,7 @@
 namespace rest {
     public class EdificateRequestDto{
+        public int userId { get; set; }
         public int squareId { get; set; }
-        public int amount { get; set; }
+        public bool intendedEdficationLevel { get; set; }
     }
 }
--- botanapoly/rest/src/model/UserJoinDto.cs
+++ botanapoly/rest/src/model/UserJoinDto.cs
@@ -1,5 +1,6 @@
 namespace rest{
     public class UserJoinDto{
-        
+        public int matchId { get; set; }
+        public string password { get; set; }
     }
 }
