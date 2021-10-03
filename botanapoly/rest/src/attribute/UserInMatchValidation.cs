a05d841d4424da51b392791413155bbdfa72ce93
g-gar <g.gar.fern@hotmail.com>
2021-10-03 12:38:26 +0200

    added user in match validation

--- botanapoly/rest/src/attribute/UserInMatchValidation.cs
+++ botanapoly/rest/src/attribute/UserInMatchValidation.cs
@@ -1,5 +1,49 @@
-﻿namespace rest{
-    public class UserInMatchValidation{
-        
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using model;
+using rest.service;
+
+namespace rest{
+    [AttributeUsage(validOn: AttributeTargets.Class | AttributeTargets.Method)]
+    public class UserInMatchValidation : Attribute, IAsyncActionFilter {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            IMatchRepository repository = Configuration.Instance.MatchRepository;
+            IApiKeyStore apiKeyStore = Configuration.Instance.ApiKeyStore;
+            HttpRequest request = context.HttpContext.Request;
+            if (context.HttpContext.Request.RouteValues.TryGetValue("matchId", out var extractedMatchID))
+            {
+                if (request.Headers.TryGetValue("ApiKey", out var extractedApiKey))
+                {
+                    int matchId = int.Parse((string) extractedMatchID);
+                    int? userId = apiKeyStore.find(extractedApiKey);
+                    if (userId.HasValue)
+                    {
+                        IEnumerable<MatchInfo> matches = repository.getMatches(match => match.Id.Equals(matchId));
+                        if (matches.Any())
+                        {
+                            MatchInfo match = matches.First();
+                            IEnumerable<PlayerInfo> players = repository.getMatchPlayers(match, player => player.User.Id.Equals(userId));
+                            if (players.Any())
+                            {
+                                await next();
+                            }
+                        }
+                    }
+                }
+            }
+            context.Result = new ContentResult()
+            {
+                StatusCode = 404,
+                Content = "user is not in requested match"
+            };
+            return;
+        }
     }
 }
