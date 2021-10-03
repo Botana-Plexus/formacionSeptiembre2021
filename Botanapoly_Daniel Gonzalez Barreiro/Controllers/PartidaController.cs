0a11766d5fe24b191d8010e4d2ebd2193cce5d15
Daniel Gonzalez Barreiro <>
2021-10-03 16:31:30 +0200

    last issues resolved

--- Botanapoly_Daniel Gonzalez Barreiro/Controllers/PartidaController.cs
+++ Botanapoly_Daniel Gonzalez Barreiro/Controllers/PartidaController.cs
@@ -80,6 +80,86 @@ namespace API_Botanapoly.Controllers
             return database.insertQuery(query);
         }
         
+        [HttpPost("mover")]
+        public string mover(int idJugador, int tirada)
+        {
+            string query;
+            query = $"mover '{idJugador}','{tirada}'";
+            System.Data.DataTable dt = database.selectQuery(query);
+            int casillaNova = Convert.ToInt32(dt.Rows[0][0]);
+
+            query = $"getCasillas null,'{casillaNova}'";
+            System.Data.DataTable dt2 = database.selectQuery(query);
+            int tipoCasilla = Convert.ToInt32(dt2.Rows[0]["tipo"]);
+
+            switch (tipoCasilla)
+            {
+                case 2:
+                case 3:
+                case 4:
+                case 8:
+                    query = $"actualizarDeuda '{idJugador}',null, '{casillaNova}'";
+
+                    return database.insertQuery(query);
+                case 1:
+                case 6:
+
+                    return "casilla neutra";
+                case 7:
+                    query = $"castigar '{idJugador}'";
+
+                    return database.insertQuery(query);
+                case 5:
+                    query = $"getCartaAleatoria '{idJugador}'";
+                    System.Data.DataTable dt3 = database.selectQuery(query);
+                    int idCarta = Convert.ToInt32(dt3.Rows[0][0]);
+
+                    query = $"getInfoCarta '{idCarta}'";
+                    System.Data.DataTable dt4 = database.selectQuery(query);
+                    int tipoCarta = Convert.ToInt32(dt4.Rows[0]["tipo"]);
+                    int valorCarta = Convert.ToInt32(dt4.Rows[0]["valor"]);
+
+                    if (tipoCarta == 2)
+                    {
+                        query = $"actualizarDeuda '{idJugador}','{idCarta}','{tirada}'";
+                        return database.insertQuery(query);
+                    }
+                    else
+                    {
+                        return mover(idJugador, valorCarta);
+                    }
+
+            }
+            return "";
+
+        }
+
+        [HttpPost("comprar")]
+        public string comprar(int idJugador)
+        {
+            string query = $"comprar '{idJugador}' ";
+            System.Data.DataTable dt = database.selectQuery(query);
+            return dt.Rows[0]["Column2"].ToString();
+        }
+
+        [HttpPost("edificar")]
+        public string edificar(int idJugador, int idCasilla)
+        {
+            string query = $"edificar '{idJugador}','{idCasilla}'";
+
+            System.Data.DataTable dt = database.selectQuery(query);
+            return dt.Rows[0]["Column2"].ToString();
+        }
+
+        [HttpPost("venderEdificacion")]
+        public string venderEdificacion(int idJugador, int idCasilla)
+        {
+            string query = $"venderEdificacion '{idJugador}','{idCasilla}'";
+
+            System.Data.DataTable dt = database.selectQuery(query);
+            return dt.Rows[0]["Column2"].ToString();
+        }
+
         [HttpPost("actualizarNivelEdificacion")]
         public string actualizarNivelEdificacion(int idJugador, int tipo)
         {
@@ -92,13 +172,6 @@ namespace API_Botanapoly.Controllers
             return database.insertQuery(query);
         }
 
-        [HttpPost("comprar")]
-        public string comprar(int idJugador)
-        {
-            string query = $"comprar '{idJugador}' ";
-            System.Data.DataTable dt = database.selectQuery(query);
-            return dt.Rows[0]["Column2"].ToString();
-        }
 
         [HttpPost("vender")]
         public string vender(int idJugador, int idCasilla)
@@ -106,6 +179,16 @@ namespace API_Botanapoly.Controllers
             string query = $"vender '{idJugador}','{idCasilla}'";
             System.Data.DataTable dt = database.selectQuery(query);
             return dt.Rows[0]["Column2"].ToString();
+        }
+
+        [HttpPost("finalizarTurno")]
+        public string finalizarTurno(int idJugador)
+        {
+            string query = $"pagarDeuda '{idJugador}'";
+
+
+            return database.insertQuery(query);
+
         }
 
         [HttpPost("mostrarTablero")]
@@ -142,6 +225,93 @@ namespace API_Botanapoly.Controllers
 
             return lista;
         }
+        
+        [HttpPost("retirarJugador")]
+        public string retirarJugador(int idJugador)
+        {
+            string consulta = $"retirarJugador '{idJugador}'";
+
+            return database.insertQuery(consulta);
+        }
+
+        [HttpPost("abandonarPartida")]
+        public string abandonarPartida(int idJugador)
+        {
+            string consulta = $"abandonarPartida '{idJugador}'";
+
+            return database.insertQuery(consulta);
+        }
+
+        
+
+        [HttpPost("setDobles")]
+        public string setDobles(int idJugador, int reset)
+        {
+            string query = $"setDobles '{idJugador}','{reset}'";
+
+            return database.insertQuery(query);
+        }      
+
+        [HttpPost("getTurno")]
+        public string getTurno(int idJugador, int idPartida)
+        {
+            string query = $"getTurno '{idPartida}','{idJugador}'";
+
+            System.Data.DataTable dt = database.selectQuery(query);
+            return dt.Rows[0]["Column2"].ToString();
+        }
+
+        [HttpPost("cartaAleatoria")]
+        public int getCartaAleatoria(int idJugador)
+        {
+            string query = $"getCartaAleatoria '{idJugador}'";
+
+            System.Data.DataTable dt = database.selectQuery(query);
+
+            return Convert.ToInt32(dt.Rows[0]["id"]);
+        }
+
+        [HttpDelete("finalizarPartida")]
+        public string finalizarPartida(int idPartida)
+        {
+            string query = $"finalizarPartida '{idPartida}'";
+
+            return database.insertQuery(query);
+        }
+        
+        [HttpPost("actualizarDeuda")]
+        public string actualizarDeuda(int idJugador, int idCarta)
+        {
+            string query = $"actualizarDeuda '{idJugador}','{idCarta}'";
+            return database.insertQuery(query);
+
+        }
+
+        [HttpPost("pagarDeuda")]
+        public string pagarDeuda(int idJugador)
+        {
+            string query = $"pagarDeuda '{idJugador}'";
+
+
+            System.Data.DataTable dt = database.selectQuery(query);
+            return dt.Rows[0]["Column2"].ToString();
+
+        }
+
+        [HttpPost("getInfoCarta")]
+        public Cartas getInfoCarta(int idCarta)
+        {
+            string query = $"getInfoCarta '{idCarta}'";
+
+            System.Data.DataTable dt = database.selectQuery(query);
+            Cartas carta = new Cartas();
+            carta.id = Convert.ToInt32(dt.Rows[0]["id"]);
+            carta.texto = Convert.ToString(dt.Rows[0]["texto"]);
+            carta.valor = Convert.ToInt32(dt.Rows[0]["valor"]);
+            carta.tipo = Convert.ToInt32(dt.Rows[0]["tipo"]);
+
+            return carta;
+        }
 
         [HttpGet("listarTableros")]
         public List<Tableros> listarTableros()
@@ -163,7 +333,7 @@ namespace API_Botanapoly.Controllers
             }
             return lista;
         }
-        
+
         [HttpGet("listarCasillas")]
         public List<Casilla> getCasillas(int idTablero, int? idCasilla)
         {
@@ -171,13 +341,14 @@ namespace API_Botanapoly.Controllers
             string query = "getCasillas";
             if (idCasilla != null)
             {
-                query += "'" + idTablero + "'"+","+ "'" + idCasilla + "'";
-            } else
+                query += "'" + idTablero + "'" + "," + "'" + idCasilla + "'";
+            }
+            else
             {
                 query += "'" + idTablero + "'";
             }
             System.Data.DataTable dt = database.selectQuery(query);
-      
+
             var lista = new List<Casilla>();
 
             for (int i = 0; i < dt.Rows.Count; i++)
@@ -187,10 +358,10 @@ namespace API_Botanapoly.Controllers
                 casilla.tipo = Convert.ToInt32(dt.Rows[i]["tipo"]);
                 casilla.nombre = Convert.ToString(dt.Rows[i]["nombre"]);
                 casilla.precioCompra = dt.Rows[i]["precioCompra"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["precioCompra"]) : null;
-                casilla.precioVenta = dt.Rows[i]["precioVenta"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["precioVenta"]) :null;
+                casilla.precioVenta = dt.Rows[i]["precioVenta"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["precioVenta"]) : null;
                 casilla.costeEdificacion = dt.Rows[i]["costeEdificacion"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["costeEdificacion"]) : null;
-                casilla.precioVentaEdificacion = dt.Rows[i]["precioVentaEdificacion"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["precioVentaEdificacion"]) :null;
-                casilla.Coste1 = dt.Rows[i]["Coste1"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["Coste1"]):null;
+                casilla.precioVentaEdificacion = dt.Rows[i]["precioVentaEdificacion"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["precioVentaEdificacion"]) : null;
+                casilla.Coste1 = dt.Rows[i]["Coste1"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["Coste1"]) : null;
                 casilla.Coste2 = dt.Rows[i]["Coste2"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["Coste2"]) : null;
                 casilla.Coste3 = dt.Rows[i]["Coste3"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["Coste3"]) : null;
                 casilla.Coste4 = dt.Rows[i]["Coste4"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["Coste4"]) : null;
@@ -205,14 +376,14 @@ namespace API_Botanapoly.Controllers
             }
             return lista;
         }
-        
+
         [HttpGet("listarPartidas")]
         public List<Partida> getPartidas(int? idPartida)
         {
             string query = "getPartidas";
             if (idPartida != null)
             {
-                query += "'" + idPartida +  "'";
+                query += "'" + idPartida + "'";
             }
             System.Data.DataTable dt = database.selectQuery(query);
 
@@ -252,10 +423,10 @@ namespace API_Botanapoly.Controllers
                 casilla.tipo = Convert.ToInt32(dt.Rows[i]["tipo"]);
                 casilla.nombre = Convert.ToString(dt.Rows[i]["nombre"]);
                 casilla.precioCompra = dt.Rows[i]["precioCompra"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["precioCompra"]) : null;
-                casilla.precioVenta = dt.Rows[i]["precioVenta"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["precioVenta"]) :null;
+                casilla.precioVenta = dt.Rows[i]["precioVenta"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["precioVenta"]) : null;
                 casilla.costeEdificacion = dt.Rows[i]["costeEdificacion"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["costeEdificacion"]) : null;
-                casilla.precioVentaEdificacion = dt.Rows[i]["precioVentaEdificacion"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["precioVentaEdificacion"]) :null;
-                casilla.Coste1 = dt.Rows[i]["Coste1"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["Coste1"]):null;
+                casilla.precioVentaEdificacion = dt.Rows[i]["precioVentaEdificacion"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["precioVentaEdificacion"]) : null;
+                casilla.Coste1 = dt.Rows[i]["Coste1"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["Coste1"]) : null;
                 casilla.Coste2 = dt.Rows[i]["Coste2"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["Coste2"]) : null;
                 casilla.Coste3 = dt.Rows[i]["Coste3"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["Coste3"]) : null;
                 casilla.Coste4 = dt.Rows[i]["Coste4"] != System.DBNull.Value ? Convert.ToInt32(dt.Rows[i]["Coste4"]) : null;
@@ -269,172 +440,7 @@ namespace API_Botanapoly.Controllers
             }
             return lista;
         }
-        
-        [HttpPost("retirarJugador")]
-        public string retirarJugador(int idJugador)
-        {
-            string consulta = $"retirarJugador '{idJugador}'";
 
-            return database.insertQuery(consulta);
-        }
-
-        [HttpPost("abandonarPartida")]
-        public string abandonarPartida(int idJugador)
-        {
-            string consulta = $"abandonarPartida '{idJugador}'";
-
-            return database.insertQuery(consulta);
-        }
-
-        [HttpPost("edificar")]
-        public string edificar(int idJugador, int idCasilla)
-        {
-            string query = $"edificar '{idJugador}','{idCasilla}'";
-
-            System.Data.DataTable dt = database.selectQuery(query);
-            return dt.Rows[0]["Column2"].ToString();
-        }
-
-        [HttpPost("venderEdificacion")]
-        public string venderEdificacion(int idJugador, int idCasilla)
-        {
-            string query = $"venderEdificacion '{idJugador}','{idCasilla}'";
-
-            System.Data.DataTable dt = database.selectQuery(query);
-            return dt.Rows[0]["Column2"].ToString();
-        }
-
-        [HttpPost("setDobles")]
-        public string setDobles(int idJugador, int reset)
-        {
-            string query = $"setDobles '{idJugador}','{reset}'";
-
-            return database.insertQuery(query);
-        }
-
-
-        [HttpPost("mover")]
-        public string mover(int idJugador, int tirada)
-        {
-            string query;
-            query = $"mover '{idJugador}','{tirada}'";
-            System.Data.DataTable dt = database.selectQuery(query);
-            int casillaNova = Convert.ToInt32(dt.Rows[0][0]);
-
-            query = $"getCasillas null,'{casillaNova}'";
-            System.Data.DataTable dt2 = database.selectQuery(query);
-            int tipoCasilla = Convert.ToInt32(dt2.Rows[0]["tipo"]);
-
-            switch (tipoCasilla)
-            {
-                case 2: case 3:
-                case 4: case 8:
-                    query = $"actualizarDeuda '{idJugador}','{casillaNova}'";
-                    
-                    return database.insertQuery(query);
-                case 1: case 6:
-
-                    return "casilla neutra";
-                case 7:
-                    query = $"castigar '{idJugador}'";
-
-                    return database.insertQuery(query);
-                case 5:
-                    query = $"getCartaAleatoria '{idJugador}'";
-                    System.Data.DataTable dt3 = database.selectQuery(query);
-                    int idCarta = Convert.ToInt32(dt3.Rows[0][0]);
-
-                    query = $"getInfoCarta '{idCarta}'";
-                    System.Data.DataTable dt4 = database.selectQuery(query);
-                    int tipoCarta = Convert.ToInt32(dt4.Rows[0]["tipo"]);
-                    int valorCarta = Convert.ToInt32(dt4.Rows[0]["valor"]);
-
-                    if(tipoCarta == 2)
-                    {
-                        query = $"actualizarDeuda '{idCarta}','{idCarta}'";
-                        return database.insertQuery(query);
-                    }
-                    else
-                    {
-                        return mover(idJugador, valorCarta);
-                    }
-
-            }
-            return "";
-
-        }
-              
-
-        [HttpPost("getTurno")]
-        public string getTurno(int idJugador, int idPartida)
-        {
-            string query = $"getTurno '{idPartida}','{idJugador}'";
-
-            System.Data.DataTable dt = database.selectQuery(query);
-            return dt.Rows[0]["Column2"].ToString();
-        }
-
-        [HttpPost("cartaAleatoria")]
-        public int getCartaAleatoria(int idJugador)
-        {
-            string query = $"getCartaAleatoria '{idJugador}'";
-
-            System.Data.DataTable dt = database.selectQuery(query);
-
-            return Convert.ToInt32(dt.Rows[0]["id"]);
-        }
-
-        [HttpPost("getInfoCarta")]
-        public Cartas getInfoCarta(int idCarta)
-        {
-            string query = $"getInfoCarta '{idCarta}'";
-
-            System.Data.DataTable dt = database.selectQuery(query);
-            Cartas carta = new Cartas();
-            carta.id = Convert.ToInt32(dt.Rows[0]["id"]);
-            carta.texto = Convert.ToString(dt.Rows[0]["texto"]);
-            carta.valor = Convert.ToInt32(dt.Rows[0]["valor"]);
-            carta.tipo = Convert.ToInt32(dt.Rows[0]["tipo"]);
-
-            return carta;
-        }
-
-        [HttpPost("finalizarPartida")]
-        public string finalizarPartida(int idPartida)
-        {
-            string query = $"finalizarPartida '{idPartida}'";
-
-            return database.insertQuery(query);
-        }
-        
-        [HttpPost("actualizarDeuda")]
-        public string actualizarDeuda(int idJugador, int idCarta)
-        {
-            string query = $"actualizarDeuda '{idJugador}','{idCarta}'";
-            return database.insertQuery(query);
-
-        }
-
-        [HttpPost("pagarDeuda")]
-        public string pagarDeuda(int idJugador)
-        {
-            string query = $"pagarDeuda '{idJugador}'";
-
-
-            System.Data.DataTable dt = database.selectQuery(query);
-            return dt.Rows[0]["Column2"].ToString();
-
-        }
-        
-        [HttpPost("finalizarTurno")]
-        public string finalizarTurno(int idJugador)
-        {
-            string query = $"pagarDeuda '{idJugador}'";
-
-
-            return database.insertQuery(query);
-
-        }
 
         //Metodo que vende todas las propiedades junto con los edificios de estas y se obtiene el jugador con más saldo(getMasRico)
         private Jugador determinarGanador(int idPartida)
